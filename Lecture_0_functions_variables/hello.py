e3be54ff81e4101a624cba224303cb6847e96a26
Miguel_M <miguelbarrosmarques@gmail.com>
2022-09-07 00:33:27 +0100

    06/09/2022

--- Lecture_0_functions_variables/hello.py
+++ Lecture_0_functions_variables/hello.py
@@ -1,3 +1,12 @@
-print ("hallo, welt")
-F_SETFDsdfsa
-sfda
+# Ask user for user name
+name = input ("What's your name? ")
+
+# Remove whitespace from string and capitalize user's name
+name = name.strip().title()
+
+# Split user's name into first name and last name
+first, last = name.split(" ")
+
+# Say hello to user
+print ("hello,", name)
+print (f"hello, {last}")
