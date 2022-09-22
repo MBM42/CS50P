7805e954574833653d72cb43057cb07ca84bf087
MBM42 <miguelbarrosmarques@gmail.com>
2022-09-22 18:33:13 +0100

    adding dictionary

---
+++ Lecture_2_loops/hogwarts_dict.py
@@ -0,0 +1,11 @@
+# Demonstrates indexing into a dict
+
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
+
+for student in students:
+    print(student, students[student], sep=", ")
